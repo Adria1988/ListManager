147629fc5838ac8e97be903c2633bd080c67a95a
Adria1988 <adribuch1988@gmail.com>
2020-03-26 20:59:22 +0100

    refactor tests

--- test/list.test.js
+++ test/list.test.js
@@ -5,39 +5,52 @@ var jsonfile = require("jsonfile");
 var List = require("../list.js");
 
 suite("List", function() {
-
   const JSON_FILENAME = "lists.json";
   var mockJsonfile = null;
   setup(function() {
-  this.mockJsonfile = sinon.mock(jsonfile);
-
+    this.mockJsonfile = sinon.mock(jsonfile);
   });
 
   test("should get all lists", function() {
     //Arrange
     var emptyLists = {};
-    this.mockJsonfile.expects('readFileSync').once().withArgs(JSON_FILENAME).returns(emptyLists);
-    var listObejct = new List(jsonfile);
+    this.mockJsonfile
+      .expects("readFileSync")
+      .once()
+      .withArgs(JSON_FILENAME)
+      .returns(emptyLists);
+    var listManager = createListManager();
     //Act
-    var result = listObejct.getLists();
+    var result = listManager.getLists();
     //Assert
-    assert.equal(emptyLists,result);
+    assert.equal(emptyLists, result);
     this.mockJsonfile.verify();
   });
 
-  test('should create a new list with name',function(){
+  test("should create a new list with name", function() {
     //Arrange
     var emptyLists = {};
-    this.mockJsonfile.expects('readFileSync').once().withArgs(JSON_FILENAME).returns(emptyLists);
+    this.mockJsonfile
+      .expects("readFileSync")
+      .once()
+      .withArgs(JSON_FILENAME)
+      .returns(emptyLists);
     var lists = {};
-    var listName = 'ToDo';
+    var listName = "ToDo";
     lists[listName] = [];
-    this.mockJsonfile.expects('writeFileSync').once().withArgs(JSON_FILENAME,lists);
-    var listObejct = new List(jsonfile);
+    this.mockJsonfile
+      .expects("writeFileSync")
+      .once()
+      .withArgs(JSON_FILENAME, lists);
+    var listManager = createListManager();
 
     //Act
-    listObejct.createList(listName);
+    listManager.createList(listName);
     //Assert
     this.mockJsonfile.verify();
   });
+
+  function createListManager(){
+    return new List(jsonfile);
+  }
 });
